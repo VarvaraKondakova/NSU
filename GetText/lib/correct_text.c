3b2dbad7fe8f8c08d6597d0d57754678ceff8d21
VarvaraKondakova <kondakovarvara@mail.ru>
2025-05-05 22:39:53 +0700

    Add GetText/

---
+++ GetText/lib/correct_text.c
@@ -0,0 +1,120 @@
+#include "correct_text.h"
+
+// Является ли буквой
+int isLetter(char symbol) {
+    return (symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z');
+}
+
+// Является ли заглавной буквой
+int isCapitalLetter(char symbol) {
+    return symbol >= 'A' && symbol <= 'Z';
+}
+
+// Является ли строчной буквой
+int isLowercaseLetter(char symbol) {
+    return symbol >= 'a' && symbol <= 'z';
+}
+
+// Меняем строчную на заглавную
+char changeLowercase(char symbol) {
+    if (isLowercaseLetter(symbol)) {
+        return symbol - 32;
+    }
+
+    return symbol;
+}
+
+// Меняем заглавную на строчную
+char changeCapital(char symbol) {
+    if (isCapitalLetter(symbol)) {
+        return symbol + 32;
+    }
+
+    return symbol;
+}
+
+// Является концом предложения
+int isSentenceEnd(char symbol) {
+    return symbol == '.' || symbol == '!' || symbol == '?';
+}
+
+// Является пробелом, табом и переходом на новую строку
+int isSpace(char symbol) {
+    return symbol == ' ' || symbol == '\t' || symbol == '\n';
+}
+
+// Является ли знаком препинания
+int isPunctuationMarks(char symbol) {
+    return symbol == '.' || symbol == '!' || symbol == '?' || symbol == ',' || symbol == ':' || symbol == ';';
+}
+
+// Удаляет char по индексу и смещает весь массив влево после удаленного символа
+void deleteChar(char *pointerText, int indexDeleteChar) {
+    for (int i = indexDeleteChar; pointerText[i] != '\0'; i++) {
+        pointerText[i] = pointerText[i + 1];
+    }
+}
+
+// Добавляет пробел и сдвигает все символы вправо начиная с indexAddSpaceChar
+int addSpaceChar(char *pointerText, int indexAddSpaceChar, size_t allocatedSize) {
+    size_t len = strlen(pointerText);
+
+    if (len >= allocatedSize - 2) {
+        return 0;
+    }
+
+    for (int i = len; i >= indexAddSpaceChar; i--) {
+        pointerText[i + 1] = pointerText[i];
+    }
+
+    pointerText[indexAddSpaceChar] = ' ';
+    return 1;
+}
+
+int correctText(char *pointerText, size_t allocatedSize) {
+    int isStartSentence = 1;
+    int isStartText = 1;
+
+    for (int i = 0; pointerText[i] != '\0'; i++) {
+        if ((isSpace(pointerText[i]) && isStartText) || (isPunctuationMarks(pointerText[i]) && isStartText)) {
+            deleteChar(pointerText, i);
+        }
+
+        if (isLetter(pointerText[i])) {
+            if (isStartSentence) {
+                pointerText[i] = changeLowercase(pointerText[i]);
+                isStartSentence = 0;
+            } else {
+                pointerText[i] = changeCapital(pointerText[i]);
+            }
+
+            isStartText = 0;
+        }
+
+        if (isSentenceEnd(pointerText[i])) {
+            isStartSentence = 1;
+        }
+
+        if (isSpace(pointerText[i]) && isSpace(pointerText[i + 1])) {
+            deleteChar(pointerText, i);
+            i--;
+            continue;
+        }
+
+        if (isSpace(pointerText[i]) && isPunctuationMarks(pointerText[i + 1])) {
+            deleteChar(pointerText, i);
+            i--;
+            continue;
+        }
+
+        if (isPunctuationMarks(pointerText[i]) && !isSpace(pointerText[i + 1])) {
+            if (!addSpaceChar(pointerText, i + 1, allocatedSize)) {
+                return 0;
+            }
+
+            continue;
+        }
+    }
+
+    return 1;
+}
---
+++ GetText/main.c
@@ -0,0 +1,174 @@
+#include "lib/correct_text.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TERMINATOR '#'
+
+void choiceStdinText(int *pointer);
+
+FILE *selectInputSource(int choice);
+
+char *myRealloc(char *oldPointer, size_t oldAllocatedSize, size_t newAllocatedSize, size_t dataSize);
+
+char *GetText(FILE *stream, char terminator);
+
+/**
+ * Реализация функции ввода текста произвольного размера: char* GetText(FILE* stream, char terminator)
+ * • Конец текста обозначается выбранным символом terminator (например, '#' или '\n')
+ * • Возвращается строка в стиле C (нуль-терминированная), не включающая символа terminator
+ * • Выделение точного количества необходимой памяти (собственная функция перевыделения памяти безиспользования realloc() )
+ * • Использование функции корректировки из задачи 9
+ * • Ввод текста из консоли или файла (выбор задается с консоли)
+ */
+int main() {
+    int choice;
+    choiceStdinText(&choice);
+
+    FILE *inputSource = selectInputSource(choice);
+    if (inputSource == NULL) {
+        fprintf(stderr, "The input source could not be opened!\n");
+        return 1;
+    }
+
+    if (choice == 1) {
+        printf("Enter the text ending in %c:\n", TERMINATOR);
+    }
+
+    char *text = GetText(inputSource, TERMINATOR);
+
+    if (choice == 2) {
+        fclose(inputSource);
+    }
+
+    if (text != NULL) {
+        printf("Edited text:\n%s\n", text);
+        free(text);
+    } else {
+        fprintf(stderr, "An error has occurred!\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+void choiceStdinText(int *pointer) {
+    while (1) {
+        printf("Choose a text input method:\n1. Text input via the console.\n2.Reading text from a file\n_: ");
+
+        if (scanf("%d", pointer) != 1 || getchar() != '\n') {
+            printf("Incorrect value was entered for text input selection.!\n");
+
+            while (getchar() != '\n');
+
+            continue;
+        }
+
+        if (*pointer != 1 && *pointer != 2) {
+            printf("Incorrect text input selection value has been entered!\n");
+
+            continue;
+        }
+
+        break;
+    }
+}
+
+FILE *selectInputSource(int choice) {
+    if (choice == 1) {
+        return stdin;
+    } else {
+        char filename[256];
+
+        printf("Enter the relative path to the file: ");
+        scanf("%255s", filename);
+
+        while (getchar() != '\n');
+
+        FILE *file = fopen(filename, "r");
+
+        if (file == NULL) {
+            fprintf(stderr, "File opening error %s\n", filename);
+            return NULL;
+        }
+
+        return file;
+    }
+}
+
+char *myRealloc(char *oldPointer, size_t oldAllocatedSize, size_t newAllocatedSize, size_t dataSize) {
+    if (newAllocatedSize < 0) {
+        return NULL;
+    }
+
+    if (newAllocatedSize == 0) {
+        free(oldPointer);
+        return NULL;
+    }
+
+    if (oldPointer == NULL) {
+        return (char *) malloc(newAllocatedSize);
+    }
+
+    char *newPointer = (char *) malloc(newAllocatedSize);
+    if (newPointer == NULL) {
+        return NULL;
+    }
+
+    size_t bytesToCopy = dataSize < newAllocatedSize ? dataSize : newAllocatedSize;
+    memcpy(newPointer, oldPointer, bytesToCopy);
+
+    free(oldPointer);
+
+    return newPointer;
+}
+
+char *GetText(FILE *stream, char terminator) {
+    size_t capacity = 100;
+    size_t sizeData = 0;
+    char ch;
+
+    char *buffer = (char *) malloc(capacity * sizeof(char));
+    if (buffer == NULL) {
+        fprintf(stderr, "Dynamic memory allocation error!\n");
+        return NULL;
+    }
+
+    while ((ch = fgetc(stream)) != terminator && ch != EOF) {
+
+        if (sizeData >= capacity - 1)
+        {
+            size_t newCapacity = capacity * 2;
+            char *temp = myRealloc(buffer, capacity, newCapacity, sizeData);
+
+            if (temp == NULL) {
+                fprintf(stderr, "Dynamic memory overallocation error!\n");
+                free(buffer);
+                return NULL;
+            }
+
+            buffer = temp;
+            capacity = newCapacity;
+        }
+
+        buffer[sizeData++] = ch;
+    }
+
+    buffer[sizeData] = '\0';
+
+    if (!correctText(buffer, capacity)) {
+        fprintf(stderr, "Couldn't edit the text!\n");
+        free(buffer);
+        return NULL;
+    }
+
+    char *result = (char *) myRealloc(buffer, capacity, sizeData + 1, sizeData);
+    if (result == NULL) {
+        fprintf(stderr, "Couldn't reduce the size of the allocated memory for the string!\n");
+        free(buffer);
+        return NULL;
+    }
+
+    return result;
+}
